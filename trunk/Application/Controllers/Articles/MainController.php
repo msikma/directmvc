8da8b23e960d4c1eefb7f9fb3ddc66790324c49f
c.minor6 <c.minor6@3e8ea682-c564-11de-81ce-f10158adbdd1>
2009-11-02 09:18:24 +0000

    Made a small example of how models 'will' work!

--- trunk/Application/Controllers/Articles/MainController.php
+++ trunk/Application/Controllers/Articles/MainController.php
@@ -3,7 +3,27 @@ class Articles_MainController extends Df_Controller
 {
 	function Index( Df_Request $request )
 	{
-		return 'Test!';
+		/**
+		* Example of fetching an article from the database
+		*/
+		$article = ArticlesModel::filter( array(
+			'pk' => 1
+		) );
+		
+		echo $article->title;
+		
+		/**
+		* Example of how we update an article
+		*/
+		$article->title = 'Updated';
+		$article->save();
+		
+		/**
+		* Example of how we create a new article and save it
+		*/
+		$new_article = ArticlesModel::createNew();
+		$new_article->title = 'A Test Article';
+		$new_article->save(); // Inserts new article into the database
 	}
 	
 	function View( Df_Request $request, $slug )
